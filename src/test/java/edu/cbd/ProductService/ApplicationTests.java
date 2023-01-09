977c0b6a26788f6e2e03cd0b679c41408b2117f0
Steven Diestel <82208488+Izamii@users.noreply.github.com>
2023-01-09 14:37:04 +0100

    Renamed and deleted unnecessary Test folder

--- src/test/java/edu/cbd/ProductService/ApplicationTests.java
+++ src/test/java/edu/cbd/ProductService/ApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.cbd.weaponservice;
+package edu.cbd.ProductService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
